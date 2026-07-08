b79854d4cfab4a305b9d6307356cc4d89aa1a6f5
khedkargayatri2802 <khedkargayatri2802@gmail.com>
2026-07-08 12:54:26 +0530

    added

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to react Application
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ciitstudent.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        CIIT Training Institude
         </a>
       </header>
     </div>
